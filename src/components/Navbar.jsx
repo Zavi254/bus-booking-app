856073e5c1e2da6c067ec68195c5082a8a05f8d5
Steven Otianga <steveotianga8991@gmail.com>
2022-10-24 16:28:57 +0300

    Added the Navbar for the home page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav
+      className="navbar navbar-expand-sm justify-content-between"
+      style={{
+        backgroundColor: "#F20C0C",
+        fontFamily: "Montserrat', sans-serif",
+        fontWeight: "700",
+      }}
+    >
+      <div className="mobileNavbar">
+        <Link to="/" className="navbar-brand text-white">
+          BUS BOOKING APP
+        </Link>
+
+        <button
+          className="navbar-toggler togglerIcon"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#demo"
+        >
+          <FaBars style={{ fontSize: "1.5rem", color: "white" }} />
+        </button>
+      </div>
+      <div>
+        <ul
+          className="navbar-nav offcanvas offcanvas-end text-bg-dark"
+          id="demo"
+        >
+          <li className="nav-item">
+            <Link to="/" className="nav-link text-white">
+              HOME
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="about" className="nav-link text-white">
+              OFFERS
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/portfolio" className="nav-link text-white">
+              ABOUT US
+            </Link>
+          </li>
+          <div className="offcanvas-header">
+            <button
+              className="btn-close btn-close-white close-button"
+              type="button"
+              data-bs-dismiss="offcanvas"
+            ></button>
+          </div>
+        </ul>
+      </div>
+
+      <Outlet />
+    </nav>
+  );
+};
+
+export default Navbar;
