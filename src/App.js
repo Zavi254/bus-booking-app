455eb18b9fe519385e64c2e7c5e3267fc925a2b7
Steven Otianga <steveotianga8991@gmail.com>
2022-10-25 04:39:01 +0300

    Changed the official path to the home page

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Homepage />} />
       </Routes>
     </BrowserRouter>
   );
