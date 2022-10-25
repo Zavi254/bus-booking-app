14511559a21bcd820763c7115b642fe8d5d3aa1e
Steven Otianga <steveotianga8991@gmail.com>
2022-10-26 01:36:46 +0300

    Added the search for available buses feature

---
+++ src/components/ModalCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const ModalCard = ({ content }) => {
+  return (
+    <>
+      <div className="modal fade" id="myModal">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Available Buses</h4>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+              ></button>
+            </div>
+
+            <div className="modal-body">
+              {content}
+            </div>
+
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ModalCard;
