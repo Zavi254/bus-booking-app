cbcb5f02109cdf21d1d49427ca92be56fb8b6750
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-10-26 17:31:53 +0300

    add landing page

---
+++ src/components/LandingImage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LandingImage = () => {
+  return <div className="Landingbus"></div>;
+};
+
+export default LandingImage;
---
+++ src/pages/Landingpage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Landing from "../components/Landing";
+import LandingImage from "../components/LandingImage";
+
+
+const Landingpage = () => {
+    return (
+      <div>
+        <LandingImage/>
+        <Navbar />
+        <Landing />
+        
+        
+      </div>
+    );
+  };
+  
+  export default Landingpage;
+  
