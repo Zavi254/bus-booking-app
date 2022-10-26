0db5744293a63887b3453881d8350b0ebdafdbb2
Steven Otianga <steveotianga8991@gmail.com>
2022-10-26 17:59:19 +0300

    changed the homepage landing page route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Homepage />} />
+        <Route path="/home" element={<Homepage />} />
       </Routes>
     </BrowserRouter>
   );
