8e9fac4752f70614e5e16212cd7698c586b6f8f6
Musyoki <vincenbryaniel@gmail.com>
2022-11-02 09:58:40 +0300

    coming up with dasboard and mainpage components

---
+++ src/Admin/Dashboard.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+// import Customers from "./Customers";
+
+
+// import Driver from "../Driver/Driver";
+// import Bookings from "./Bookings";
+// import Drivers from "./Drivers";
+// import Buses from "./Buses";
+// import Admin from "./Admin";
+function Dashboard() {
+    return (
+        <>
+            <div className="container ">
+                <div className="row row-cols-1 row-cols-md-3 g-4">
+                    <div className="col">
+                        <div className="card h-100" style={{backgroundColor:"#07D2FF"}}>
+                            {/* <img src="..." className="card-img-top" alt="..." /> */}
+                            <div className="card-body">
+                                <h5 className="card-title">0</h5>
+                                <p className="card-text">Total Bookings</p>
+                            </div>
+                            <a href="/bookings" className="small-box-footer" style={{textDecoration: 'none', color:'white'}}>More info <i className="bi bi-arrow-right-circle-fill" /></a>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card h-100" style={{backgroundColor:"#18BD28"}}>
+                            {/* <img src="..." className="card-img-top" alt="..." /> */}
+                            <div className="card-body">
+                                <h5 className="card-title">0</h5>
+                                <p className="card-text">Total Buses</p>
+                            </div>
+                            <a href="buses" className="small-box-footer" style={{textDecoration: 'none', color:'white'}}>More info <i className="bi bi-arrow-right-circle-fill" /></a>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card h-100" style={{backgroundColor:"#FFC01D"}}>
+                            {/* <img src="..." className="card-img-top" alt="..." /> */}
+                            <div className="card-body">
+                                <h5 className="card-title">0</h5>
+                                <p className="card-text">Total Customers</p>
+                            </div>
+                            <a href="customers" className="small-box-footer" style={{textDecoration: 'none', color:'black'}}>More info <i className="bi bi-arrow-right-circle-fill" /></a>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card h-100" style={{backgroundColor:"#A764FC"}}>
+                            {/* <img src="..." className="card-img-top" alt="..." /> */}
+                            <div className="card-body">
+                                <h5 className="card-title">0</h5>
+                                <p className="card-text">Total Drivers</p>
+                            </div>
+                            <a href="drivers" className="small-box-footer" style={{textDecoration: 'none', color:'white'}}>More info <i className="bi bi-arrow-right-circle-fill" /></a>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card h-100" style={{backgroundColor:"#FF0A18"}}>
+                            {/* <img src="..." className="card-img-top" alt="..." /> */}
+                            <div className="card-body">
+                                <h5 className="card-title">0</h5>
+                                <p className="card-text">Total Routes </p>
+                            </div>
+                            <a href="routes" className="small-box-footer" style={{textDecoration: 'none', color:'white'}}>More info <i className="bi bi-arrow-right-circle-fill" /></a>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card h-100" style={{backgroundColor:"blue"}}>
+                            <img src="..." className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                {/* <h5 className="card-title">0</h5> */}
+                                <p className="card-text">Add Admin</p>
+                            </div>
+                            <a href="admin" className="small-box-footer" style={{textDecoration: 'none', color:'white'}}>More info <i className="bi bi-arrow-right-circle-fill" /></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Dashboard;
---
+++ src/Admin/MainPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from './Navbar';
+import NavPage from './NavPage';
+import Sidebar from './Sidebar';
+function MainPage() {
+  return (
+    <>
+      {/* navbar section */}
+      <div>
+        <Navbar/>
+      </div>
+      {/* sidebar section */}
+      <div className='sidebar-container'>
+        <div className='side-bar'>
+          <Sidebar/>
+        </div>
+        <div className='nav-page'>
+          <NavPage/>
+        </div>
+      </div>
+    </>
+  )
+}
+export default MainPage;
